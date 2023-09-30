6500c7d934c4cbb801faacdc3c965cc586ccf2d3
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2023-09-30 15:33:25 -0300

    Register page updated

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,37 +1,100 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./style.module.css";
 import logo from "../../img/logo.svg";
+import profile from "../../img/addPhoto.svg";
 
 function App() {
+  const navigate = useNavigate();
   const handleRegister = async (e) => {
-    alert("Register");
+    navigate("/home");
   };
 
   return (
     <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <div className={styles.LeftSide}>
-          <div className={styles.LogoContainer}>
-            <img src={logo} alt="" />
-          </div>
+      <div className={styles.Header}>
+        <div className={styles.HeaderTitle}>
+          <img className={styles.HeaderLogo} src={logo} alt="" />
+          <div className={styles.HeaderName}>HarmonyHue</div>
         </div>
-        <div className={styles.RightSide}>
+        <div className={styles.HeaderButton}>
+          <div className={styles.HeaderText}>Are you a member?</div>
+          <button className={styles.HeaderLogin} onClick={() => navigate("/")}>
+            Get In
+          </button>
+        </div>
+      </div>
+      <div className={styles.Main}>
+        <form className={styles.Form} onSubmit={handleRegister}>
+          <div className={styles.FormField}>
+            <label htmlFor="username">Username</label>
+            <input
+              className={styles.FormInput}
+              type="text"
+              id="username"
+              name="username"
+              //value=""
+              onChange={null}
+              //required
+            />
+          </div>
+          <div className={styles.FormField}>
+            <label htmlFor="email">Email</label>
+            <input
+              className={styles.FormInput}
+              type="email"
+              id="email"
+              name="email"
+              //value=""
+              onChange={null}
+              //required
+            />
+          </div>
+          <div className={styles.FormField}>
+            <label htmlFor="dob">Date of Birth</label>
+            <input
+              className={styles.FormInput}
+              type="date"
+              id="dob"
+              name="dob"
+              // value=""
+              onChange={null}
+              //required
+            />
+          </div>
+          <div className={styles.FormField}>
+            <label htmlFor="password">Password</label>
+            <input
+              className={styles.FormInput}
+              type="password"
+              id="password"
+              name="password"
+              //value=""
+              onChange={null}
+              //required
+            />
+          </div>
+          <div className={styles.FormField}>
+            <label htmlFor="passwordConf">Confirm Password</label>
+            <input
+              className={styles.FormInput}
+              type="password"
+              id="passwordConf"
+              name="passwordConf"
+              // value=""
+              onChange={null}
+              //required
+            />
+          </div>
           <div>
-            <h1 className={styles.Title}>Register</h1>
-            <form className={styles.Form} onSubmit={handleRegister}>
-              <input className={styles.TextBox} type="text" name="email" placeholder="E-mail" required />
-              <input className={styles.TextBox} type="password" name="pass" placeholder="Password" required />
-              <input className={styles.TextBox} type="text" name="username" placeholder="Username" />
-              <input className={styles.Button} type="submit" value="Sign Up" />
-            </form>
-            <div className={styles.LinkField}>
-              <Link to={"/"} className={styles.Link}>
-                Already have an account? Login
-              </Link>
-            </div>
+            <button className={styles.FormButton} type="submit">
+              Register
+            </button>
           </div>
+        </form>
+        <div>
+          <img className={styles.Profile} src={profile} alt="" />
         </div>
-      </header>
+      </div>
     </div>
   );
 }
