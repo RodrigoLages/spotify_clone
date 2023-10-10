32b47ea0306a18ce7f76bb67758a04a07cc25118
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2023-10-10 16:56:41 -0300

    Login working with mocked data

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 import logo from "../../img/logo.svg";
 
 function App() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
   const handleLogin = async (e) => {
-    alert("Login");
+    const user = mockData.find((element) => element.user === email);
+    if (user && user.pass === pass) {
+      navigate("/home");
+    } else {
+      alert("User invalid");
+    }
   };
+
+  const mockData = [
+    {
+      user: "bigas@email.com",
+      pass: "1234",
+    },
+  ];
 
   return (
     <div className={styles.App}>
@@ -19,8 +38,26 @@ function App() {
           <div>
             <h1 className={styles.Title}>HarmonyHue</h1>
             <form className={styles.Form} onSubmit={handleLogin}>
-              <input className={styles.TextBox} type="text" name="email" placeholder="E-mail" required />
-              <input className={styles.TextBox} type="password" name="pass" placeholder="Password" required />
+              <input
+                className={styles.TextBox}
+                type="text"
+                name="email"
+                placeholder="E-mail"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                className={styles.TextBox}
+                type="password"
+                name="pass"
+                placeholder="Password"
+                required
+                onChange={(e) => {
+                  setPass(e.target.value);
+                }}
+              />
               <input className={styles.Button} type="submit" value="Get In" />
             </form>
             <div className={styles.LinkField}>
