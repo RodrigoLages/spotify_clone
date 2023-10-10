9297f428f454c32d0dda9c13a7dd484f28aaa8f4
Gabriela Borborema <gabrielakborborema@gmail.com>
2023-10-10 16:34:33 -0300

    dynamic playlist in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,62 @@
 import styles from "./style.module.css";
+
+import { useNavigate } from "react-router-dom";
+
 import logo from "../../img/LogoNav.svg";
 import iconSetaDireita from "../../img/icon_seta_direita.svg"
 import iconSetaEsquerda from "../../img/icon_seta_esquerda.svg"
 import iconSearch from "../../img/Search.svg"
 import imagePerfil from "../../img/Perfil.png"
+
 import imageLiked from "../../img/Muc curtidas.png"
+import imageFavEp from "../../img/Ep Favoritos.png"
+import imageAudioBook from "../../img/Audio Book.png"
+import imageGoodVibe from "../../img/Good Vibes.png"
+import imageTreino from "../../img/Treino em casa.png"
+import imagePagode from "../../img/Hora do Pagode.png"
+import imageRock from "../../img/Rock.png"
+
 import musicCard from "../../img/Card  1.png"
 
+const playlists = [
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+  {
+    image: musicCard,
+    title: "Lofi Sleep",
+    desciption: "Lofi Sleep"
+  },
+]
+
 function App() {
+  const navigate = useNavigate();
+  const handleRegister = async (e) => {
+    navigate("/playlist");
+  };
+
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
@@ -33,24 +82,21 @@ function App() {
             <p className={styles.Title}>Good afternoon</p>
             <div className={styles.Line}>
               <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
-              <img src={imageLiked} alt="" />
+              <img src={imageFavEp} alt="" />
+              <img src={imageAudioBook} alt="" />
+              <img src={imageGoodVibe} alt="" />
+              <img src={imageTreino} alt="" />
+              <img src={imagePagode} alt="" />
+              <img src={imageRock} alt="" />
             </div>
             <div className={styles.LineText}>
               <p className={styles.Title}>Sweet Dreams</p>
               <button className={styles.ShowButton}>Show all</button>
             </div>
             <div className={styles.Line}>
-              <img src={musicCard} alt="" />
-              <img src={musicCard} alt="" />
-              <img src={musicCard} alt="" />
-              <img src={musicCard} alt="" />
-              <img src={musicCard} alt="" />
-              <img src={musicCard} alt="" />
+            {playlists.map((playlist, i) => (
+              <button className={styles.CardButton} onClick={handleRegister}><img src={playlist.image} alt="" /></button>
+          ))}
             </div>
             <div className={styles.LineText}>
               <p className={styles.Title}>Sweet Dreams</p>
